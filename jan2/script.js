060f72f2dc0a019d0f2d242770914cf117e85745
Dhanvanth Sathyamangalam Balakrishnan <dhanvanth.balakrishnan@int.trustrace.com>
2025-01-03 17:02:28 +0530

    jan3 class

--- jan2/script.js
+++ jan2/script.js
@@ -39,19 +39,13 @@ function task1fun(e) {
   const len = val.length;
   if (len >= task1MaxLimit) {
     e.preventDefault();
-  }
-  else if((len >= 2 && val.charAt(len-1) === ' ' && e.key === ' ')){
+  } else if (len >= 2 && val.charAt(len - 1) === " " && e.key === " ") {
     e.preventDefault();
     e.target.value = val.trim();
-    
-  }
-  else if(len === 0 || (len >= 2 && val.charAt(len-1) === ' ')){
+  } else if (len === 0 || (len >= 2 && val.charAt(len - 1) === " ")) {
     e.preventDefault();
     e.target.value = val + e.key.toUpperCase();
   }
-
-  
-
 }
 
 function pTagLength(e) {
@@ -60,15 +54,15 @@ function pTagLength(e) {
   pTag.textContent = `Num of Count allowed : ${20 - len}`;
 }
 
-function timeString(e){
-    e.target.value = e.target.value.trim();
-    pTagLength(e)
+function timeString(e) {
+  e.target.value = e.target.value.trim();
+  pTagLength(e);
 }
 
 function asciiValue(str) {
+  if (str.length > 1) return -1;
   return str.charCodeAt(0);
 }
-
 
 //------------------------------------------------------------------
 const task2 = document.getElementById("task2");
@@ -80,23 +74,27 @@ function task2fun(e) {
   }
   const val = e.target.value;
   const len = val.length;
-  if(len === 0) {
+  if (len === 0) {
     e.preventDefault();
-    if(e.key.length === 1 && ((ascciNum >= a && ascciNum <= z) || (ascciNum >= A && ascciNum <= Z))){
-        e.target.value = e.key.toUpperCase()
+    if (
+      e.key.length === 1 &&
+      ((ascciNum >= a && ascciNum <= z) || (ascciNum >= A && ascciNum <= Z))
+    ) {
+      e.target.value = e.key.toUpperCase();
+    }
+  } else {
+    e.preventDefault();
+    if (
+      e.key.length === 1 &&
+      ((ascciNum >= a && ascciNum <= z) || (ascciNum >= A && ascciNum <= Z))
+    ) {
+      e.target.value = val + e.key.toLowerCase();
     }
   }
-  else{
-    e.preventDefault();
-    if(e.key.length === 1 && ((ascciNum >= a && ascciNum <= z) || (ascciNum >= A && ascciNum <= Z))){
-        e.target.value = val + e.key.toLowerCase();
-    }
-  }
-//   e.target.value = `${val.charAt(0).toUpperCase()}${val
-//     .substring(1)
-//     .toLowerCase()}`;
+  //   e.target.value = `${val.charAt(0).toUpperCase()}${val
+  //     .substring(1)
+  //     .toLowerCase()}`;
 }
-
 
 //--------------------------------------------------------------------------
 const task4 = document.getElementById("task4");
@@ -111,9 +109,10 @@ function task4fun(e) {
     )
   ) {
     e.preventDefault();
+  } else {
+    e.preventDefault();
+    e.target.value = e.target.value + e.key.toUpperCase();
   }
-  const val = e.target.value;
-  e.target.value = val.toUpperCase();
 }
 
 //----------------------------------------------------------------------------------
@@ -149,5 +148,102 @@ function task3fun(e) {
     }
   }
 }
+//-----------------------------------------------------------------------------
 
+const task6 = document.getElementById("task6");
+task6.addEventListener("keydown", task6fun);
 
+function task6fun(e) {
+  const val = e.target.value;
+  const len = val.length;
+  const index = task6.selectionStart;
+  const key = asciiValue(e.key);
+  if (e.key.length > 1) {
+    if (
+      e.key.toUpperCase() === "BACKSPACE" &&
+      ((index > 2 &&
+        val.charAt(index - 2) === " " &&
+        val.charAt(index) !== " ") ||
+        index === 1)
+    ) {
+      if (index < len) {
+        e.preventDefault();
+        e.target.value =
+          val.substring(0, index - 1) +
+          val.charAt(index).toUpperCase() +
+          val.substring(index + 1);
+        task6.setSelectionRange(index - 1, index - 1);
+      }
+    } else if (
+      e.key.toUpperCase() === "BACKSPACE" &&
+      val.charAt(index - 1) === " "
+    ) {
+      e.preventDefault();
+      e.target.value =
+        val.substring(0, index - 1) + makeSmall(val.substring(index));
+      task6.setSelectionRange(index - 1, index - 1);
+    } else if (
+      e.key.toUpperCase() === "BACKSPACE" &&
+      val.charAt(index - 2) === " " &&
+      val.charAt(index) === " "
+    ) {
+      e.preventDefault();
+      e.target.value = val.substring(0, index - 2) + val.substring(index);
+      task6.setSelectionRange(index - 1, index - 1);
+    } else if (e.key.toUpperCase() === "BACKSPACE" && index === 1) {
+      e.preventDefault();
+      e.target.value = val.substring(0, index - 2) + val.substring(index);
+      task6.setSelectionRange(index - 1, index - 1);
+    }
+  } else {
+    // console.log(task6.selectionStart);
+    if (key === space && index === 0) {
+      e.preventDefault();
+    } else if (
+      key === space &&
+      (val.charAt(index - 1) === " " || val.charAt(index) === " ")
+    ) {
+      e.preventDefault();
+      if (val.charAt(index) === " ") {
+        task6.setSelectionRange(index + 1, index + 1);
+      }
+    } else if (
+      key === space &&
+      index < len &&
+      isAlpahbet(asciiValue(val.charAt(index - 1))) &&
+      isAlpahbet(asciiValue(val.charAt(index)))
+    ) {
+      e.preventDefault();
+      e.target.value =
+        val.substring(0, index) +
+        " " +
+        val.charAt(index).toUpperCase() +
+        val.substring(index + 1);
+      task6.setSelectionRange(index + 1, index + 1);
+    } else if (key === space) {
+    } else if (!isAlpahbet(key)) {
+      e.preventDefault();
+    } else if (index === 0 && isAlpahbet(key)) {
+      e.preventDefault();
+      e.target.value = e.key.toUpperCase() + val;
+      task6.setSelectionRange(1, 1);
+    } else if (
+      val.charAt(len - 1) === " " &&
+      isAlpahbet(key) &&
+      len === index
+    ) {
+      e.preventDefault();
+      e.target.value = val + e.key.toUpperCase();
+    }
+  }
+}
+
+function isAlpahbet(key) {
+  return (key >= a && key <= z) || (key >= A && key <= Z) ? true : false;
+}
+
+function makeSmall(str) {
+  const array = str.split(" ");
+  array[0] = array[0].toLowerCase();
+  return array.join(" ");
+}
