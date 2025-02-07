29e6e19c8b4127e76fdd011eeae3bf3795ec50cc
Dhanvanth Sathyamangalam Balakrishnan <dhanvanth.balakrishnan@int.trustrace.com>
2025-02-07 17:53:03 +0530

    under development survey time

---
+++ google-form/admin-page/script.js
@@ -0,0 +1,20 @@
+import { sideBarInit } from "./side-bar.js"
+import { navBarInit } from "./nav-bar.js"
+import { surveyListInit } from "./survey-list.js"
+
+
+//initializing the navBar
+navBarInit();
+
+const div = document.createElement('div');
+div.className = 'body-container';
+document.body.appendChild(div);
+
+//initializing the sideBar, appending the sideBar to the div
+sideBarInit(div);
+
+const page = document.createElement('div');
+page.className = 'page';
+div.appendChild(page);
+
+
